4eab1faf74b036d5fa07289c85151c6318ce8b38
Paul Edward Bennett <paul.e.bennett@icloud.com>
2019-03-25 09:04:22 +0000

    Fix issue with line56 in settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhitenoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blog.urls'
